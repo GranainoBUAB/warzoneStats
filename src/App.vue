a22b5608458be4437c4b21db076208bcce33506c
Granaino BUAB <josemiguel@quesada.eu>
2021-08-20 19:43:37 +0200

    dchange backgroud color

--- src/App.vue
+++ src/App.vue
@@ -21,11 +21,13 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: black;
+  background-color:  darkgrey;
 }
 
 html, body {
   font-family: 'Roboto', sans-serif;
+  background-color: darkgrey;
 }
 
 #nav {
